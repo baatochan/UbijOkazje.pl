4ad992bb3c68eff0966dab4362badd9ebce33414
Bartosz Rodziewicz <bartoszka1996@gmail.com>
2018-01-21 20:23:40 +0100

    New order for css and js files

--- index.php
+++ index.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <link href="imports/bootstrap.min.css" rel="stylesheet">
-    <link href="main.css" rel="stylesheet">
+    <link href="css/index.css" rel="stylesheet">
     <title>UbijOkazje.pl - najlepsze okazje w sieci!</title>
     <script src="imports/jquery-3.1.1.slim.min.js"></script>
     <script src="imports/tether.min.js"></script>
     <script src="imports/bootstrap.min.js"></script>
-    <script src="main.js"></script>
+    <script src="js/index.js"></script>
 </head>
 <body>
 <div id="logLink"><a href="login.php">Zaloguj sie</a></div>
