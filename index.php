6f061630dce927f72356ccb0a59959e6e9f14567
Bartosz Rodziewicz <bartoszka1996@gmail.com>
2018-01-21 19:42:20 +0100

    Add "nedded" imports
    Because one cannot simply do a web page in 2018 without bootsrap and jquerry XDDD

--- index.php
+++ index.php
@@ -2,9 +2,15 @@
 <html lang="pl">
 <head>
     <meta charset="UTF-8">
-    <title>Hello world!</title>
+    <link href="imports/bootstrap.min.css" rel="stylesheet">
+    <link href="main.css" rel="stylesheet">
+    <title>UbijOkazje.pl - najlepsze okazje w sieci!</title>
+    <script src="imports/jquery-3.1.1.slim.min.js"></script>
+    <script src="imports/tether.min.js"></script>
+    <script src="imports/bootstrap.min.js"></script>
+    <script src="main.js"></script>
 </head>
 <body>
-<p>Hello fucking world.</p>
+<div></div>
 </body>
 </html>
