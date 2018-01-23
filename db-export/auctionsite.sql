6d9bab6ae19e626b476bb2300bac49eb26511354
Bartosz Rodziewicz <bartoszka1996@gmail.com>
2018-01-23 18:24:46 +0100

    Update db export

--- db-export/auctionsite.sql
+++ db-export/auctionsite.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jan 23, 2018 at 10:42 AM
+-- Generation Time: Jan 23, 2018 at 05:23 PM
 -- Server version: 5.7.19
 -- PHP Version: 5.6.31
 
@@ -33,12 +33,13 @@ USE `auctionsite`;
 DROP TABLE IF EXISTS `address`;
 CREATE TABLE IF NOT EXISTS `address` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `Street` varchar(100) COLLATE utf8_bin DEFAULT NULL,
-  `Number` int(11) DEFAULT NULL,
-  `Code` varchar(10) COLLATE utf8_bin DEFAULT NULL,
-  `City` varchar(100) COLLATE utf8_bin DEFAULT NULL,
-  `Country` varchar(30) COLLATE utf8_bin DEFAULT NULL,
-  PRIMARY KEY (`Id`)
+  `Street` varchar(100) COLLATE utf8_bin NOT NULL,
+  `Number` int(11) NOT NULL,
+  `Code` varchar(10) COLLATE utf8_bin NOT NULL,
+  `City` varchar(100) COLLATE utf8_bin NOT NULL,
+  `Country` varchar(30) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `Id` (`Id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
@@ -58,12 +59,13 @@ INSERT INTO `address` (`Id`, `Street`, `Number`, `Code`, `City`, `Country`) VALU
 DROP TABLE IF EXISTS `comment`;
 CREATE TABLE IF NOT EXISTS `comment` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `UserId` int(11) DEFAULT NULL,
-  `Text` varchar(2000) COLLATE utf8_bin DEFAULT NULL,
-  `Date` date DEFAULT NULL,
-  `authorId` int(11) DEFAULT NULL,
-  PRIMARY KEY (`Id`)
-) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `UserId` int(11) NOT NULL,
+  `Text` varchar(2000) COLLATE utf8_bin NOT NULL,
+  `Date` date NOT NULL,
+  `authorId` int(11) NOT NULL,
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `Id` (`Id`)
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Dumping data for table `comment`
@@ -71,7 +73,8 @@ CREATE TABLE IF NOT EXISTS `comment` (
 
 INSERT INTO `comment` (`Id`, `UserId`, `Text`, `Date`, `authorId`) VALUES
 (6, 6, 'sdafgsdfagasdg', '2018-01-23', 1),
-(7, 8, 'Zaufany sprzedajacy. Polecam!', '2018-01-23', 6);
+(7, 8, 'Zaufany sprzedajacy. Polecam!', '2018-01-23', 6),
+(8, 6, 'Testowy komentarz', '2018-01-23', 8);
 
 -- --------------------------------------------------------
 
@@ -82,10 +85,11 @@ INSERT INTO `comment` (`Id`, `UserId`, `Text`, `Date`, `authorId`) VALUES
 DROP TABLE IF EXISTS `desiredproduct`;
 CREATE TABLE IF NOT EXISTS `desiredproduct` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `ProductId` int(11) DEFAULT NULL,
-  `UserId` int(11) DEFAULT NULL,
+  `ProductId` int(11) NOT NULL,
+  `UserId` int(11) NOT NULL,
   `isHidden` tinyint(1) NOT NULL,
-  PRIMARY KEY (`Id`)
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `Id` (`Id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
@@ -107,12 +111,13 @@ INSERT INTO `desiredproduct` (`Id`, `ProductId`, `UserId`, `isHidden`) VALUES
 DROP TABLE IF EXISTS `orderedproduct`;
 CREATE TABLE IF NOT EXISTS `orderedproduct` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `ProductId` int(11) DEFAULT NULL,
-  `UserId` int(11) DEFAULT NULL,
-  `dateOfOrder` date DEFAULT NULL,
-  `isPaid` tinyint(1) DEFAULT NULL,
-  PRIMARY KEY (`Id`)
-) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `ProductId` int(11) NOT NULL,
+  `UserId` int(11) NOT NULL,
+  `dateOfOrder` date NOT NULL,
+  `isPaid` tinyint(1) NOT NULL,
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `Id` (`Id`)
+) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Dumping data for table `orderedproduct`
@@ -122,7 +127,8 @@ INSERT INTO `orderedproduct` (`Id`, `ProductId`, `UserId`, `dateOfOrder`, `isPai
 (14, 12, 6, '2018-01-23', 0),
 (13, 9, 9, '2018-01-23', 0),
 (12, 10, 8, '2018-01-23', 0),
-(11, 7, 8, '2018-01-23', 1);
+(11, 7, 8, '2018-01-23', 1),
+(15, 14, 8, '2018-01-23', 1);
 
 -- --------------------------------------------------------
 
@@ -133,15 +139,16 @@ INSERT INTO `orderedproduct` (`Id`, `ProductId`, `UserId`, `dateOfOrder`, `isPai
 DROP TABLE IF EXISTS `product`;
 CREATE TABLE IF NOT EXISTS `product` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `Name` varchar(69) COLLATE utf8_bin DEFAULT NULL,
+  `Name` varchar(69) COLLATE utf8_bin NOT NULL,
   `Photo` varchar(420) COLLATE utf8_bin DEFAULT NULL,
   `Description` varchar(2137) COLLATE utf8_bin DEFAULT NULL,
-  `Date` date DEFAULT NULL,
-  `Value` double DEFAULT NULL,
-  `Rating` int(11) DEFAULT NULL,
-  `SellerId` int(11) DEFAULT NULL,
-  PRIMARY KEY (`Id`)
-) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `Date` date NOT NULL,
+  `Value` double NOT NULL,
+  `Rating` int(11) NOT NULL,
+  `SellerId` int(11) NOT NULL,
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `Id` (`Id`)
+) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Dumping data for table `product`
@@ -153,7 +160,9 @@ INSERT INTO `product` (`Id`, `Name`, `Photo`, `Description`, `Date`, `Value`, `R
 (9, 'test3', '', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris euismod ex et magna varius, quis commodo mauris tristique. Donec vel egestas nulla, tincidunt rhoncus nibh. Cras non rutrum ipsum. Suspendisse at nisl et purus gravida lobortis. Donec non consectetur justo, aliquet commodo quam. Curabitur ac interdum turpis. Suspendisse potenti. Nam rutrum felis id ornare cursus. Aenean eget neque magna. Phasellus a scelerisque dui, sed luctus purus. Nullam sed lectus vel elit viverra vehicula. Nunc ac euismod lacus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque at neque blandit, fringilla tellus ac, placerat lectus. Donec in elit tellus. Praesent volutpat lorem nec quam suscipit, eu tincidunt ipsum accumsan. ', '2018-01-23', 222, 4, 6),
 (10, 'test4', '', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris euismod ex et magna varius, quis commodo mauris tristique. Donec vel egestas nulla, tincidunt rhoncus nibh. Cras non rutrum ipsum. Suspendisse at nisl et purus gravida lobortis. Donec non consectetur justo, aliquet commodo quam. Curabitur ac interdum turpis. Suspendisse potenti. Nam rutrum felis id ornare cursus. Aenean eget neque magna. Phasellus a scelerisque dui, sed luctus purus. Nullam sed lectus vel elit viverra vehicula. Nunc ac euismod lacus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque at neque blandit, fringilla tellus ac, placerat lectus. Donec in elit tellus. Praesent volutpat lorem nec quam suscipit, eu tincidunt ipsum accumsan. ', '2018-01-23', 123.33, 2, 6),
 (11, 'test5', 'https://i.pinimg.com/564x/60/4b/74/604b740adb143b8fb0a0e97bcbfdf017--mlp-twilight-twilight-sparkle.jpg', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris euismod ex et magna varius, quis commodo mauris tristique. Donec vel egestas nulla, tincidunt rhoncus nibh. Cras non rutrum ipsum. Suspendisse at nisl et purus gravida lobortis. Donec non consectetur justo, aliquet commodo quam. Curabitur ac interdum turpis. Suspendisse potenti. Nam rutrum felis id ornare cursus. Aenean eget neque magna. Phasellus a scelerisque dui, sed luctus purus. Nullam sed lectus vel elit viverra vehicula. Nunc ac euismod lacus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque at neque blandit, fringilla tellus ac, placerat lectus. Donec in elit tellus. Praesent volutpat lorem nec quam suscipit, eu tincidunt ipsum accumsan. ', '2018-01-23', 22.45, 5, 6),
-(12, 'test6', '', '', '2018-01-23', 333.44, 2, 8);
+(12, 'test6', '', '', '2018-01-23', 333.44, 2, 8),
+(13, 'test8', '', 'kirmkfgklt,gl kt,golfl,gl; kgdokfl', '2018-01-23', -50, 0, 11),
+(14, 'fgfgfg', 'http://', 'dwdfwfr', '2018-01-23', 5.454646, 1, 11);
 
 -- --------------------------------------------------------
 
@@ -164,15 +173,16 @@ INSERT INTO `product` (`Id`, `Name`, `Photo`, `Description`, `Date`, `Value`, `R
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `AddressId` int(11) DEFAULT NULL,
-  `FirstName` varchar(50) COLLATE utf8_bin DEFAULT NULL,
-  `LastName` varchar(50) COLLATE utf8_bin DEFAULT NULL,
+  `AddressId` int(11) NOT NULL,
+  `FirstName` varchar(50) COLLATE utf8_bin NOT NULL,
+  `LastName` varchar(50) COLLATE utf8_bin NOT NULL,
   `Username` varchar(50) COLLATE utf8_bin NOT NULL,
-  `Email` varchar(100) COLLATE utf8_bin DEFAULT NULL,
+  `Email` varchar(100) COLLATE utf8_bin NOT NULL,
   `SaltyPassword` varchar(100) COLLATE utf8_bin NOT NULL,
   `Salt` varchar(6) COLLATE utf8_bin NOT NULL,
-  PRIMARY KEY (`Id`)
-) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `Id` (`Id`)
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Dumping data for table `user`
@@ -182,7 +192,8 @@ INSERT INTO `user` (`Id`, `AddressId`, `FirstName`, `LastName`, `Username`, `Ema
 (9, 7, 'test', 'test', 'yyy', 'yyy@test.pl', 'b3e0086c40c00b27bfb13610b833fafe', '656254'),
 (6, 5, 'Bartosz', 'Rodziewicz', 'baatochan', 'baatochan@test.pl', 'ea74800e9f333adc83914704385ec0fe', '3998d7'),
 (8, 7, 'Test', 'test', 'xxx', 'xxx@test.pl', 'abf5244351167128fdd67eeb494f7f98', '2abcc2'),
-(10, 7, 'Test', 'Test', 'zzz', 'zzz@test.pl', '11bffdbeccfa13fcb6909d4ec30502c6', 'e68973');
+(10, 7, 'Test', 'Test', 'zzz', 'zzz@test.pl', '11bffdbeccfa13fcb6909d4ec30502c6', 'e68973'),
+(11, 7, 'test', 'test', 'aaa', 'xxx@www.pl', '4f841624b54d743606b3a977ae3a35e7', '07b9e8');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
